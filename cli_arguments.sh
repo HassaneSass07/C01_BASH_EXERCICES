98258731528307f006df5b10ab2d6cd7815f7252
idriss <idrissaamadouousmane@gmail.com>
2025-05-14 23:42:08 +0100

    aqwc

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -9,8 +9,8 @@ i=1
 
 # Parcours du tableau 'arguments' avec une boucle
 for arg in "${arguments[@]}"; do
-    echo "Argument $i : $arg"  # Affiche l'indice et la valeur
-    ((i++))  # Incrémente le compteur
+    echo "Argument $((i++)): $arg"  # Affiche l'indice et la valeur
+      # Incrémente le compteur
 done
 
 
