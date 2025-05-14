80e71924d40d3fcc946461669b9c66f5825426d8
idriss <idrissaamadouousmane@gmail.com>
2025-05-14 23:20:54 +0100

    esd

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-i=1
+i=1  # Initialisation de la variable i à 1, qui servira à compter les arguments
+
+# Boucle for pour parcourir tous les arguments passés au script
 for arg in "$@"; do
-    echo "Argument $i : $arg"
-    ((i++))
+    echo "Argument $i : $arg"  # Affichage de l'argument actuel avec son numéro (i)
+    ((i++))  # Incrémentation de la variable i pour le prochain argument
 done
 
