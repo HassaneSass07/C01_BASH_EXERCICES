be25ad465996a4e32da3bd1507aafa811da14849
idriss <idrissaamadouousmane@gmail.com>
2025-05-14 19:49:05 +0100

    xcvb

--- show_disk_usage.sh
+++ show_disk_usage.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 # Script pour afficher l'utilisation du disque
-
-# Affichage général de l'utilisation du disque
-echo "=== Utilisation globale du disque ==="
 df -h
 
-echo ""
 
-# Optionnel : Affichage de l'utilisation d'un dossier spécifique si fourni
-if [ -n "$1" ]; then
-    if [ -d "$1" ]; then
-        echo "=== Utilisation du dossier '$1' ==="
-        du -sh "$1"
-    else
-        echo "Erreur : '$1' n'est pas un dossier valide."
-    fi
-else
-    echo "Astuce : Vous pouvez aussi passer un dossier en argument pour voir son espace utilisé."
-    echo "Exemple : ./show_disk_usage.sh /home"
-fi
+
+
--- simple_calculator.sh
+++ simple_calculator.sh
@@ -1,46 +1,18 @@
 #!/bin/bash
-# Script pour effectuer une opération simple : addition, soustraction, multiplication ou division
 
-# Vérifier que l'utilisateur a bien fourni 3 arguments
-if [ $# -ne 3 ]; then
-    echo "Usage : $0 nombre1 nombre2 opérateur(+ - * /)"
-    exit 1
+
+read a b op
+
+if [ "$op" = "+" ];then 
+  resultat=$(($a + $b))
+
+elif [ "$op" = "-" ];then 
+  resultat=$(($a - $b))
+
+elif [ "$op" = "*" ];then 
+  resultat=$(($a * $b))
+
+elif [ "$op" = "/" ];then 
+  resultat=$(($a / $b))
 fi
-
-# Récupération des arguments
-num1=$1
-num2=$2
-op=$3
-
-# Vérification que les deux premiers arguments sont des nombres (entiers ou flottants)
-if ! [[ "$num1" =~ ^-?[0-9]+(\.[0-9]+)?$ ]] || ! [[ "$num2" =~ ^-?[0-9]+(\.[0-9]+)?$ ]]; then
-    echo "Erreur : Les deux premiers arguments doivent être des nombres."
-    exit 0
-fi
-
-# Calcul selon l'opérateur fourni
-case "$op" in
-    +)
-        result=$(echo "$num1 + $num2" | bc)
-        ;;
-    -)
-        result=$(echo "$num1 - $num2" | bc)
-        ;;
-    \*)
-        result=$(echo "$num1 * $num2" | bc)
-        ;;
-    /)
-        if [ "$num2" == 0 ]; then
-            echo "Erreur : Division par zéro"
-            exit 0
-        fi
-        result=$(echo "scale=2; $num1 / $num2" | bc)
-        ;;
-    *)
-        echo "Erreur : Opérateur invalide. Utilisez +, -, *, ou /"
-        exit 0
-        ;;
-esac
-
-# Affichage du résultat
-echo "Résultat : $result"
+ echo "resultat : $resultat"
